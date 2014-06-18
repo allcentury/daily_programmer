5415f156ed6b3238ab7122483f7a3ec4d0e7b654
Anthony Ross <anthony.s.ross@gmail.com>
2014-06-18 10:37:10 -0700

    initial draft of poker game

---
+++ poker/lib/poker.rb
@@ -0,0 +1,140 @@
+require 'pry'
+class Card
+  attr_reader :value, :suit
+  
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
+
+  def rank
+    ranks = {'J' => 11, 'Q' => 12, 'K'=> 13, 'A'=>14}
+    if ranks.include?(self.value)
+      return ranks[self.value]
+    else
+      return self.value.to_i
+    end
+  end
+  
+end
+
+class Deck
+
+  def new_game
+    deck = []
+    suits = ['spades', 'hearts', 'clubs', 'diamonds']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits.each do |suit|
+      values.each do |value|
+        deck << Card.new(value, suit)
+      end
+    end
+    deck
+  end
+
+end
+
+class Hand
+  attr_reader :cards
+  
+  def initialize(cards)
+    @cards = cards
+  end
+  
+  def royal_flush?
+    suits = [] 
+    vals = []
+    rf = ['A', 'K', 'Q', 'J', '10']
+    @cards.each { |card| suits << card.suit }
+    @cards.each { |card| vals << card.value }
+    perms = vals.permutation.to_a
+    if perms.include?(rf) && suits.uniq.count == 1
+      return true
+    end
+    false
+  end
+  
+  def straight_flush?
+    current_suits = suits
+    current_vals = values.keys
+    current_vals.map {|val| val.to_i}.sort!
+    prev_val = current_vals.first.to_i
+    (1..4).each do |ctr|
+      if current_vals[ctr].to_i - prev_val.to_i != 1 
+        return false
+      end
+      prev_val = current_vals[ctr]
+    end
+    true
+  end
+
+  def four_of_a_kind?
+    current_vals = values
+    current_vals.each do |k, v|
+      return true if v == 4
+    end
+    false
+  end
+
+  def full_house?
+    if three_of_a_kind? && pair?
+      return true
+    end
+    false
+  end
+
+  def three_of_a_kind?
+    current_vals = values
+    current_vals.each do |k, v|
+      return true if v == 3
+    end
+    false
+  end
+
+  def pair?
+    current_vals = values
+    current_vals.each do |k, v|
+      return true if v == 2
+    end
+    false
+  end
+
+  def flush?
+    return true if suits.keys.uniq.count == 1
+    false
+  end
+
+  def straight?
+    current_vals = values.to_a
+    prev_val = current_vals.first[0].to_i
+    (1..4).each do |ctr|
+      if current_vals[ctr][0].to_i - prev_val != 1 
+        return false
+      end
+      prev_val = current_vals[ctr][0].to_i
+    end
+    true
+  end
+
+  def high_card
+    vals = values.keys.sort_by { |k, v| v }.last.to_i
+  end
+
+  private
+
+  def suits
+    results = Hash.new 0
+    @cards.each do |card|
+      results[card.suit] += 1
+    end
+    results
+  end
+  
+  def values
+    results = Hash.new 0
+    @cards.each do |card|
+      results[card.rank.to_s] += 1
+    end
+    results
+  end
+end
---
+++ poker/spec/poker_spec.rb
@@ -0,0 +1,112 @@
+require 'rspec'
+require_relative '../lib/poker'
+
+
+describe Card do
+  let(:card) { Card.new('A', 'Spades') }
+  it 'has a value and a type' do
+    expect(card.value).to eq('A')
+    expect(card.suit).to eq('Spades')
+  end
+  it 'has a rank associated with each card' do
+    expect(card.rank).to eq(14)
+  end
+  it 'has an implied rank when not a face card' do
+    card1 = Card.new('10', 'Hearts')
+    card2 = Card.new('7', 'Diamonds')
+    expect(card1.rank).to eq(10)
+    expect(card2.rank).to eq(7)
+  end
+end
+
+describe Deck do
+  it 'deals 52 unique cards' do
+    deck = Deck.new
+    deck.new_game
+    expect(deck.new_game.count).to eq(52)
+    expect(deck.new_game.uniq.count).to eq(52)
+  end
+end
+
+describe Hand do
+  it 'validates a royal flush' do 
+    suit = 'spades'
+    cards = []
+    vals = ['A', 'K', 'Q', 'J', '10']
+   vals.each do |val|
+     cards << Card.new(val, suit)
+   end
+   hand = Hand.new(cards)
+   expect(hand.royal_flush?).to be(true)
+  end
+  it 'validates a straight flush' do
+    suit = 'spades'
+    cards = []
+    vals = ['4', '5', '6', '7', '8']
+    vals.each do |val|
+      cards << Card.new(val, suit)
+    end
+    hand = Hand.new(cards)
+    expect(hand.straight_flush?).to be(true)
+  end
+  it 'validats a straight flush with face cards' do 
+    suit = 'spades'
+    cards = []
+    vals = ['8', '9', '10', 'J', 'Q']
+    vals.each do |val|
+      cards << Card.new(val, suit)
+    end
+    hand = Hand.new(cards)
+    expect(hand.straight_flush?).to be(true)
+  end
+  it 'validates a four of a kind' do
+    cards = []
+    cards.push(Card.new('Q', 'Spades'), Card.new('Q', 'Hearts'), Card.new('Q', 'Clubs'), Card.new('Q', 'Diamonds'), Card.new('5', 'Spades'))
+    hand = Hand.new(cards)
+    expect(hand.four_of_a_kind?).to be(true)
+  end
+  it 'validates a full house' do
+    cards = []
+    cards.push(Card.new('K', 'Spades'), Card.new('K', 'Hearts'), Card.new('3', 'Spades'), Card.new('3', 'Hearts'), Card.new('3', 'Diamonds'))
+    hand = Hand.new(cards)
+    expect(hand.full_house?).to be(true)
+  end
+  it 'validates a three of a kind' do
+    cards = []
+    cards.push(Card.new('8', 'Spades'), Card.new('8', 'Hearts'), Card.new('8', 'Diamonds'), Card.new('J', 'Spades'), Card.new('5', 'Diamonds'))
+    hand = Hand.new(cards)
+    expect(hand.three_of_a_kind?).to be(true)
+  end
+  it 'validates a pair' do
+    cards = []
+    cards.push(Card.new('8', 'Spades'), Card.new('8', 'Hearts'), Card.new('5', 'Hearts'), Card.new('4', 'Clubs'), Card.new('7', 'Hearts'))
+    hand = Hand.new(cards)
+    expect(hand.pair?).to be(true)
+  end
+  it 'validtes a flush' do
+    cards = []
+    suit = 'Diamonds'
+    vals = ['A', 'Q', '6', 'J', '2']
+    vals.each do |val|
+      cards << Card.new(val, suit)
+    end
+    hand = Hand.new(cards)
+    expect(hand.flush?).to be(true)
+  end
+  it 'validates a straight' do
+    cards = []
+    cards.push(Card.new('9', 'Spades'), Card.new('10', 'Hearts'), Card.new('J', 'Hearts'), Card.new('Q', 'Diamonds'), Card.new('K', 'Spades'))
+    hand = Hand.new(cards)
+    expect(hand.straight?).to be(true)
+  end
+  it 'validates a high card' do
+    cards = []
+    suit = 'Diamonds'
+    vals = ['2', '5', '8', '9', '10']
+    vals.each do |val|
+      cards << Card.new(val, suit)
+    end
+    hand = Hand.new(cards)
+    expect(hand.high_card).to eq(10)
+  end
+end
