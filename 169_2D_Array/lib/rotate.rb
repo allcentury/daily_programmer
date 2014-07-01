39942dae7d8a6d992c0b00f2d28cf8e35ebdf645
Anthony Ross <anthony.s.ross@gmail.com>
2014-07-01 15:44:51 -0400

    Rotating 2d array 90 degrees & 180 degrees

---
+++ 169_2D_Array/lib/rotate.rb
@@ -0,0 +1,28 @@
+class Rotate
+
+  def initialize(array)
+    @current_array = array
+    @side = @current_array.first.length
+  end
+
+  def ninety_degrees
+    new_array = Array.new(@side) { Array.new(@side) }
+    for x in 0..@current_array.length-1
+      temp = 0
+      y = @side-1
+      while y >= 0
+        new_array[x][y] = @current_array[temp][x]
+        y -= 1
+        temp += 1
+      end
+    end
+    @current_array = new_array
+    new_array
+  end
+
+  def one_hundred_eighty_degrees
+    2.times { self.ninety_degrees }
+    @current_array
+  end
+  
+end
